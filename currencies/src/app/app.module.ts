88381675355ac404b77edad90c2acf0f1946efd1
David Padilla <davidpaag2002@gmail.com>
2023-04-18 21:21:02 +0200

    feat(api-configuration): Some changes to manage API requests

--- currencies/src/app/app.module.ts
+++ currencies/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ApiAuthInterceptor } from './services/auth/api-auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ApiAuthInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- currencies/src/environments/environment.ts
+++ currencies/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  endpoint: 'http://localhost:8080'
+  endpoint: 'http://localhost:8080',
+  authToken: ''
 };
 
 /*
