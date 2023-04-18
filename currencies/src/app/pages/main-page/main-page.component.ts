9884601c4aa78f24aa09c4951848a9d6c1f5d848
David Padilla <davidpaag2002@gmail.com>
2023-04-18 21:21:43 +0200

    feat(main-page): Added card to subscribe to currencies

--- currencies/src/app/pages/main-page/main-page.component.ts
+++ currencies/src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CoreService } from 'src/app/services/core';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  showCurrencies = false;
+  currencyList = [
+    { currencyCode: "USD", name: "American dollar" },
+    { currencyCode: "JPY", name: "Japanese yen" },
+    { currencyCode: "GBP", name: "Pound sterling" },
+    { currencyCode: "AUD", name: "Australian dollar" },
+    { currencyCode: "CHF", name: "Swiss franc" },
+    { currencyCode: "CNH", name: "Chinese renminbi" },
+  ];
+
+  constructor(private core: CoreService) { }
 
   ngOnInit(): void {
   }
 
+  subscribeCurrency(currency: string) {
+    console.log(currency);
+
+    this.core.api.user.userSubscribeCurrencyPatch({ body: { currency } }).subscribe({
+      next: res => {
+        console.log(res);
+        
+      },
+      error: err => {
+        console.error(err);
+      }
+    })
+
+    this.showCurrencies = false;
+  }
 }
