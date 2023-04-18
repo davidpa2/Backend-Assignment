e0fe0fd6ce12b773b8927c04a33ba8277a5d37f5
David Padilla <davidpaag2002@gmail.com>
2023-04-18 16:46:26 +0200

    feat(main-page): Basic module and routing structure to start working

--- currencies/src/app/app.module.ts
+++ currencies/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
